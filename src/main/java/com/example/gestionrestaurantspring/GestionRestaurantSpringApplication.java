32e20263e4de3d1a0eaed1f76971f85a026a484b
unknown <ysfmh2002@gmail.com>
2024-01-15 16:50:52 +0100

    first commit

---
+++ src/main/java/com/example/gestionrestaurantspring/GestionRestaurantSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionrestaurantspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRestaurantSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionRestaurantSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gestionrestaurantspring/GestionRestaurantSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionrestaurantspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRestaurantSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
